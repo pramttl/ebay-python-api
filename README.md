61fec2d98657da0ddeec8232da0c882e27348d6b
Johannes Degn <j@degn.de>
2012-01-07 16:10:05 +0100

    calls to ebay work

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This is my Implementation of https://github.com/roopeshvaddepally/python-ebay
 
 #
 for now I am only adding trading api xml calls which was lacking in the original implementation
+If you want to use this rather than https://github.com/roopeshvaddepally/python-ebay, please be sure to change the config.ini which might contain my sandbox user and token depending on if I forget to take them out.
 
 #License
 The original authors published their work under the Apache License, Version 2.0 which you may see here: http://www.apache.org/licenses/LICENSE-2.0.html
--- trading.py
+++ trading.py
@@ -2,10 +2,12 @@ from configparser import ConfigParser
 import httplib2
 from urllib.parse import urlencode
 from xml.dom.minidom import parse, parseString
+from lxml import etree
 import struct
 
 def main():
-  print(getCategories())
+  request = Request()
+  print(request.trading())
 
 def addListing(options):
   #xml = buildxml(options)
@@ -19,61 +21,218 @@ def getCategories():
 
 
 class Request:
+  """
+    Class assists in making requests to the ebay trading api
+    Workhore function is Request.trading
+  """
   def __init__(self):
     config = ConfigParser()
     config.read('config.ini')
     self.app_id = config.get('keys', 'app_name')
-    self.dev_name config.get('keys', 'dev_name')
-    self.cert_name config.get('keys', 'cert_name')
+    self.dev_name = config.get('keys', 'dev_name')
+    self.cert_name = config.get('keys', 'cert_name')
     self.site_id = config.get('call', 'siteid')
     self.version = config.get('call', 'compatibility_level')
-    self.endpoint = config.get('endpoints', 'shopping')
+    self.endpoint = config.get('endpoints', 'trading')
 
     self.auth_token = config.get('auth', 'token')
 
+  def dict2xml(self, data):
+    xml = ""
+    for key in data:
+      if isinstance(data[key], dict):
+        xml = xml + '<' + key + '>' + self.dict2xml(data[key]) + '</' + key.split()[0] + '>'
+      else:
+        if data[key]: # if data[key] is empty we assume a self closing tag
+          xml = xml + '<' + key + '>' + data[key] + '</' + key + '>'
+        else:
+          xml = xml + '<' + key.split()[0] + ' />'
+    return xml
+
+
+    return xml
   def construct_xml(self, request = {}):
-    request.update({
-      'type': {'name': 'GeteBayOfficialTimeRequest', 'content': 'xmlns="urn:ebay:apis:eBLBaseComponents"'},
-      'RequesterCredentials': {'eBayAuthToken': self.auth_token},
-      'Version': '747'
-    })
-    defaultXml = '<?xml version="1.0" encoding="utf-8"?><'+request['type']['name']+' '+request['type']['content']+'><RequesterCredentials><eBayAuthToken>'+request['RequesterCredentials']['eBayAuthToken']+'</eBayAuthToken></RequesterCredentials><Version>'+request['Version']+'</Version></'+request['type']['name']+'>'
-    dom = parseString(defaultXml)
-     
+    if len(request.keys()) == 1: # we assume that we have a parent object - the request must be complete. This is of course bs
+      data = request
+      defaultXml = self.dict2xml(data)
+      dom = etree.fromsting(defaultXml)
+
+    else: # need to find out what to do if we have no parent object. For now well call for the official time...
+      data = {'GeteBayOfficialTimeRequest xmlns="urn:ebay:apis:eBLBaseComponents"': {
+        'RequesterCredentials': {'eBayAuthToken': self.auth_token},
+        'Version': '747'
+      }}
+      defaultXml = self.dict2xml(data)
+      dom = etree.fromstring(defaultXml)
+      print(self.dict2xml(request))
+      if request:
+        for key in request:
+          print (etree.fromstring(self.dict2xml({key: request[key]})))
+          #dom.documentElement.appendChild(parseString(self.dict2xml({key: request[key]}))) # add request as a child
+
+    #print ('DOM:\n', etree.tostring(dom).decode('UTF-8'))
     return dom
 
-  def construct_header(self, header):
+  def construct_header(self, header, request):
     defaultHeader = {
       'X-EBAY-API-COMPATIBILITY-LEVEL': self.version,
-      'X-EBAY-API-DEV-NAME': 
-        
+      'X-EBAY-API-DEV-NAME': self.dev_name,
+      'X-EBAY-API-APP-NAME': self.app_id,
+      'X-EBAY-API-CERT-NAME': self.cert_name,
+      'X-EBAY-API-CALL-NAME': request,
+      'X-EBAY-API-SITEID': self.site_id,
+      'Content-Type': 'text/xml',
+      'X-EBAY-API-RESPONSE-ENCODING': 'JSON'
     }
+    defaultHeader.update(header)
+    return defaultHeader
 
-  def trading(self, params = {}, headers={}, method="POST"):
+  def trading(self, params = {}, headers = {}, method = "POST"):
     """
-      sends a request to the ebay trading api via xml
+      sends a request to the ebay trading api via xml (but you can also use get if it should be necessary)
     """
-    data = dict(appid = self.app_id, siteid = self.site_id, version = self.version)
-    data.update(params)
-    headers = dict()
-    headers.update(headers)
+    data = self.construct_xml({'stuff': {'muff': 'puff'}, 'ruff': {'fluxx': 'cruff'}})
+    request = data.tag.replace('Request', '')
+    print('request:', request)
+    headers = self.construct_header(headers, request) 
     h = httplib2.Http(".cache")
-    if data and method=="GET":
-      url = endpoint + '?' + urlencode(data)
-    elif data and method=="POST":
-      url = endpoint
-      body = urlencode(data)
+    #if data and method=="GET":
+    #  url = self.endpoint + '?' + urlencode(data)
+    #elif data and method=="POST":
+    #url = self.endpoint
+    body = data
 
-
-    resp, content = h.request(url, method=method, headers=headers, body=body)
-    print (resp)
-    print (content)
     print (url)
+    print ('headers:\n', headers, '\nxml:\n', etree.tostring(body)decode('UTF-8'))
+
+    resp, content = h.request(url, method=method, headers=headers, body=etree.tostring(body))
+    #print (resp
+    #print (content)
+    #print (url)
+    #print (header)
+    return resp, content
+
+
+  def GeteBayOfficialTime():
+    request = Request()
+    return request.trading()
+  def AddFixedPriceItem:
+    pass
+  def AddItem:
+    pass
+  def AddItems:
+    pass
+  def AddToItemDescription:
+    pass
+  def CompleteSale:
+    pass
+  def ConfirmIdentity:
+    pass
+  def DeleteMyMessages:
+    pass
+  def EndFixedPriceItem:
+    pass
+  def EndItem:
+    pass
+  def EndItems:
+    pass
+  def FetchToken:
+    pass
+  def GetAccount:
+    pass
+  def GetAdFormatLeads:
+    pass
+  def GetAllBidders:
+    pass
+  def GetApiAccessRules:
+    pass
+  def GetFeedback:
+    pass
+  def GetItem:
+    pass
+  def GetItemRecommendations:
+    pass
+  def GetItemsAwaitingFeedback:
+    pass
+  def GetItemShipping:
+    pass
+  def GetItemTransactions:
+    pass
+  def GetMemberMessages:
+    pass
+  def GetMyeBaySelling:
+    pass
+  def GetOrders:
+    pass
+  def GetOrderTransactions:
+    pass
+  def GetPopularKeywords:
+    pass
+  def GetSellerDashboard:
+    pass
+  def GetSellerEvents: 
+    pass
+  def GetSellerList:
+    pass
+  def GetSellerPayments:
+    pass
+  def GetSellerTransactions:
+    pass
+  def GetSessionID:
+    pass
+  def GetStore:
+    pass
+  def GetTokenStatus:
+    pass
+  def GetUser:
+    pass
+  def GetUserContactDetails:
+    pass
+  def GetUserDisputes:
+    pass
+  def RelistFixedPriceItem:
+    pass
+  def RelistItem:
+    pass
+  def RespondToFeedback:
+    pass
+  def RespondToWantItNowPost:
+    pass
+  def ReviseCheckoutStatus:
+    pass
+  def ReviseFixedPriceItem:
+    pass
+  def ReviseInventoryStatus:
+    pass
+  def ReviseItem:
+    pass
+  def RevokeToken:
+    pass
+  def SendInvoice:
+    pass
+  def SetPromotionalSale:
+    pass
+  def SetPromotionalSaleListings:
+    pass
+  def SetShippingDiscountProfiles:
+    pass
+  def SetStore:
+    pass
+  def SetStoreCategories:
+    pass
+  def SetTaxTable:
+    pass
+  def ValidateTestUserRegistration:
+    pass
+  def VerifyAddFixedPriceItem:
+    pass
+  def VerifyAddItem:
+    pass
+  def VerifyAddSecondChanceItem:
+    pass
+  def VerifyRelistItem:
+    pass
 
 
 if __name__ == '__main__':
-  request = Request()
-  #print(dir(request.construct_xml({"määh": "muuh"})))
-  print(request.construct_xml({"määh": "muuh"}).toxml())
-
   main()
